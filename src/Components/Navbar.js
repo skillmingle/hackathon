efa5bf8ec4ed9e5383821446d3875a593e4e3f4c
unknown <dreamgaming848@gmail.com>
2024-11-03 02:02:28 +0530

    6th commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,10 +14,21 @@ const Navbar = ({ teamName, activeKey }) => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  const tagStyle = {
+    display: 'inline-block',
+    backgroundColor: '#f44336', // Red background for the tag
+    color: '#fff',
+    fontSize: '0.75em',
+    fontWeight: 'bold',
+    padding: '3px 8px',
+    borderRadius: '5px',
+    marginLeft: '10px',
+    textTransform: 'uppercase',
+  };
 
   return (
     <div className="navbar">
-      {!isMobile? <img src={leftIcon} alt="Left Icon" className="navbar-icon left-icon" />:<img src={smIcon} alt="Left Icon" className="navbar-icon left-icon" />}
+      <span>{!isMobile? <img src={leftIcon} alt="Left Icon" className="navbar-icon left-icon" />:<img src={smIcon} alt="Left Icon" className="navbar-icon left-icon" />}<span style={tagStyle}>Beta 1.1</span></span>
       <h1 className="navbar-title">{!isMobile && <span style={{color:'#3e98c7'}}>{teamName} { " | " }</span>} <span style={{color:'green'}}>{activeKey}</span></h1>
       {!isMobile && <img src={rightIcon} alt="Right Icon" className="navbar-icon right-icon" />}
     </div>
